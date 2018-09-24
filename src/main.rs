d43afb4ee6b58158d9ef046c622d0994153733a9
miyamoen <akihito.biblioteca@gmail.com>
2018-09-24 23:15:05 +0900

    実行できるようになった

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,27 @@
 use std::process::{Command, Stdio};
 
 fn main() {
-    let output = Command::new("elm.cmd")
+    let output = Command::new("sh")
         .current_dir("../test-nire")
-        // .arg("init")
-        // .stdout(Stdio::piped())
-        .stdin(Stdio::piped())
+        .arg(get_elm_path())
+        .arg("init")
+        .arg("--help")
         .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .stdin(Stdio::inherit())
         .output()
         .expect("failed to execute process");
 
-    println!("test");
-    println!("{:?}", output.status);
-    println!("{}", std::str::from_utf8(&output.stdout).unwrap());
-    println!("{}", std::str::from_utf8(&output.stderr).unwrap());
-    println!("aftertest");
+    // println!("status {:?}", output.status);
+    // println!("stdout : {}", std::str::from_utf8(&output.stdout).unwrap());
+    // println!("stderr : {}", std::str::from_utf8(&output.stderr).unwrap());
+}
+
+fn get_elm_path() -> String {
+    let output = Command::new("which")
+        .arg("elm")
+        .output()
+        .expect("failed to execute process of get_elm_path");
+
+    std::str::from_utf8(&output.stdout).unwrap().to_string()
 }
